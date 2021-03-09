92c55b739349734bbad90f49ff4fbca1c7fa3445
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2021-03-09 09:48:34 +0100

    Update utils-0.1.0.js

--- utils-0.1.0.js
+++ utils-0.1.0.js
@@ -1,80 +1,96 @@
-(function(R){
-	var _={};
+(function(root,O){
+	var _tS=function(a){
+			return O.prototype.toString.call(a);
+		},
+		_={};
 	
-	// [0.1.0]	Boolean		_.all ( Collection collection , Function test [ , Any context = root ] )
-	//						-> Boolean test ( Variable item , Number index , Collection collection )
+	// [0.1.0] Boolean _.all ( Collection collection , Function test [ , Any context = root ] )
+	// -> Boolean test ( Variable item , Number index , Collection collection )
 	_.all=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=-1,l=a.length;
 			while(++i<l){
-				if(!f.call(c,a[i],i,a){
+				if(!f.call(c,a[i],i,a)){
 					return false;
 				}
 			}
 			return true;
 		}
-		return false;
 	};
 	
-	// [0.1.0]	Boolean		_.all.right ( Collection collection , Function test [ , Any context = root ] )
-	//						-> Boolean test ( Variable item , Number index , Collection collection )
+	// [0.1.0] Boolean _.all.right ( Collection collection , Function test [ , Any context = root ] )
+	// -> Boolean test ( Variable item , Number index , Collection collection )
 	_.all.right=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=a.length;
 			while(--i>-1){
-				if(!f.call(c,a[i],i,a){
+				if(!f.call(c,a[i],i,a)){
 					return false;
 				}
 			}
 			return true;
 		}
-		return false;
 	};
 	
-	// [x.x.x]	Boolean		_.any ( Collection collection , Function test [ , Any context =root ] )
-	//						-> Boolean test ( Variable item , Number index , Collection collection )
+	// [0.1.0] Boolean _.any ( Collection collection , Function test [ , Any context =root ] )
+	// -> Boolean test ( Variable item , Number index , Collection collection )
 	_.any=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=-1,l=a.length;
 			while(++i<l){
 				if(f.call(c,a[i],i,a)){
 					return true;
 				}
 			}
+			return false;
 		}
-		return false;
 	};
 	
-	// [0.1.0]	Boolean		_.any.right ( Collection collection , Function test [ , Any context =root ] )
-	//						-> Boolean test ( Variable item , Number index , Collection collection )
+	// [0.1.0] Boolean _.any.right ( Collection collection , Function test [ , Any context =root ] )
+	// -> Boolean test ( Variable item , Number index , Collection collection )
 	_.any.right=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=a.length;
 			while(--i>-1){
 				if(f.call(c,a[i],i,a)){
 					return true;
 				}
 			}
+			return false;
 		}
-		return false;
+		
 	};
 	
-	// [x.x.x]	Function	_.arrow ( String expression )
+	// [x.x.x] Function _.arrow ( String expression )
 	
-	// [x.x.x]	Number		_.avg ( Collection collection [ , Function getter = _.identity ] )
+	// [x.x.x] Number _.avg ( Collection collection [ , Function getter = _.identity ] )
+	// [x.x.x] Number _.average ( Collection collection [ , Function getter = _.identity ] )
+	_.avg=_.average=function(a,f){
+		var r=0;
+		if(_.isCollection(a)){
+			f=_.getter(f);
+			var i=-1,l=a.length;
+			while(++i<l){
+				r+=f(a[i]);
+			}
+			r/=l;
+		}
+		return r;
+	};
 	
-	// [0.1.0]	Variable	_.const ( Any value )
-	_["const"]=function(a){
+	// [0.1.0] Variable _.const ( Any value )
+	// [0.1.0] Variable _.constant ( Any value )
+	_["const"]=_.constant=function(a){
 		return function(){
 			return a;
 		};
 	};
 	
-	// [0.1.0]	Number		_.count ( Collection collection , Function counter [ , Any context = root ] )
+	// [0.1.0] Number _.count ( Collection collection , Function counter [ , Any context = root ] )
 	_.count=function(a,f,c){
 		var r=0;
 		if(_.isCollection(a)){
@@ -88,11 +104,11 @@
 		return r;
 	};
 	
-	// [x.x.x]	Void		_.each ( Collection collection , Function iterator [ , Any context = root ] )
-	//						-> Boolean iterator ( Variable item , Number index , Collection collection )
+	// [0.1.0] Void _.each ( Collection collection , Function iterator [ , Any context = root ] )
+	// -> Boolean iterator ( Variable item , Number index , Collection collection )
 	_.each=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=-1,l=a.length;
 			while(++i<l){
 				if(false===f.call(c,a[i],i,a)){
@@ -102,14 +118,14 @@
 		}
 	};
 	
-	// [x.x.x]	Void		_.each.key ( Object object , Function iterator [ , Any context = root ] )
-	//						-> Boolean iterator ( Variable item , String index , Collection collection )
+	// [x.x.x] Void _.each.key ( Object object , Function iterator [ , Any context = root ] )
+	// -> Boolean iterator ( Variable item , String index , Collection collection )
 	
-	// [x.x.x]	Void		_.each.right ( Collection collection , Function iterator [ , Any context = root ] )
-	//						-> Boolean iterator ( Variable item , Number index , Collection collection )
+	// [x.x.x] Void _.each.right ( Collection collection , Function iterator [ , Any context = root ] )
+	// -> Boolean iterator ( Variable item , Number index , Collection collection )
 	_.each.right=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=a.length;
 			while(--i>-1){
 				if(false===f.call(c,a[i],i,a)){
@@ -119,18 +135,22 @@
 		}
 	};
 	
-	// [x.x.x]	Void		_.extend ( Object extended , Object extender [ , Boolean preserve = false ] )
+	// [0.1.0] Object _.extend ( Object extended , Object extender [ , Boolean preserve = false ] )
+	_.extend=function(a,b,p){
+		
+	};
 	
-	// [x.x.x]	Boolean		_.false ( )
+	// [0.1.0] Boolean _.false ( )
 	_["false"]=function(){
 		return false;
 	};
 	
-	// [x.x.x]	Array		_.filter ( Collection collection , Function test [ , Anyh context = root ] )
+	// [0.1.0] Array _.filter ( Collection collection , Function test [ , Anyh context = root ] )
+	// -> Boolean test ( Variable value , Number index , Collection collection )
 	_.filter=function(a,f,c){
 		var r=[];
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=-1,l=a.length;
 			while(++i<l){
 				if(f.call(c,a[i],i,a)){
@@ -141,11 +161,12 @@
 		return r;
 	};
 	
-	// [x.x.x]	Array		_.filter.right ( Collection collection , Function test [ , Anyh context = root ] )
+	// [0.1.0] Array _.filter.right ( Collection collection , Function test [ , Anyh context = root ] )
+	// -> Boolean test ( Variable value , Number index , Collection collection )
 	_.filter.right=function(a,f,c){
 		var r=[];
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=a.length;
 			while(--i>-1){
 				if(f.call(c,a[i],i,a)){
@@ -156,11 +177,11 @@
 		return r;
 	};
 	
-	// [0.1.0]	Variable	_.find ( Collection collection , Function test [ , Any context = root ] )
-	//						-> test ( Variable value , Number index , Collection collection )
+	// [0.1.0] Variable _.find ( Collection collection , Function test [ , Any context = root ] )
+	// -> test ( Variable value , Number index , Collection collection )
 	_.find=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=-1,l=a.length;
 			while(++i<l){
 				if(f.call(c,a[i],i,a)){
@@ -170,10 +191,11 @@
 		}
 	};
 	
-	// [x.x.x]	Variable	_.find.last ( Collection collection , Function test [ , Any context = root ] )
+	// [0.1.0] Variable _.find.last ( Collection collection , Function test [ , Any context = root ] )
+	// -> Boolean test ( Variable value , Number index , Collection collection )
 	_.find.last=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=a.length;
 			while(--i>-1){
 				if(f.call(c,a[i],i,a)){
@@ -183,65 +205,63 @@
 		}
 	};
 	
-	// [x.x.x]	Variable	_.get ( Any value , Number index )
-	// [x.x.x]	Variable	_.get ( Any value , String path )
+	// [x.x.x] Variable _.get ( Any value , Number index )
+	// [x.x.x] Variable _.get ( Any value , String path )
 	
-	// [x.x.x]	Function	_.getter ( Function getter )
-	// [x.x.x]	Function	_.getter ( Number index )
-	// [x.x.x]	Function	_.getter ( String path )
+	// [x.x.x] Function _.getter ( Function getter )
+	// [x.x.x] Function _.getter ( Number index )
+	// [x.x.x] Function _.getter ( String path )
 	_.getter=function(a){
-		switch(_.typeOf(a)){
-			case"Function":return a;
-			case"Number":return new Function("a","return a["+a+"];");
-			case"String":return new Function("a","return a."+a+";");
-			default:return _.identity;
-		}
+		
 	};
 	
-	// [x.x.x]	Variable	_.identity ( Any value )
+	// [0.1.0] Variable _.identity ( Any value )
 	_.identity=function(a){
 		return a;
 	};
 	
-	// [x.x.x]	Boolean		_.isArguments ( Any value )
+	// [x.x.x] Boolean _.isArguments ( Any value )
 	
-	// [x.x.x]	Boolean		_.isArray ( Any value )
+	// [x.x.x] Boolean _.isArray ( Any value )
 	
-	// [x.x.x]	Boolean		_.isBoolean ( Any value )
+	// [x.x.x] Boolean _.isBoolean ( Any value )
 	
-	// [x.x.x]	Boolean		_.isCollection ( Any value )
+	// [x.x.x] Boolean _.isCollection ( Any value )
 	
-	// [x.x.x]	Boolean		_.isDefined ( Any value )
+	// [x.x.x] Boolean _.isDefined ( Any value )
 	
-	// [x.x.x]	Boolean		_.isError ( Any value )
+	// [x.x.x] Boolean _.isError ( Any value )
 	
-	// [x.x.x]	Boolean		_.isFalsey ( Any value )
+	// [x.x.x] Boolean _.isFalsey ( Any value )
 	
-	// [x.x.x]	Boolean		_.isFunction ( Any value )
+	// [x.x.x] Boolean _.isFunction ( Any value )
 	
-	// [x.x.x]	Boolean		_.isNaN ( Any value )
+	// [x.x.x] Boolean _.isNaN ( Any value )
 	
-	// [x.x.x]	Boolean		_.isNull ( Any value )
+	// [x.x.x] Boolean _.isNull ( Any value )
 	
-	// [x.x.x]	Boolean		_.isNumber ( Any value )
+	// [x.x.x] Boolean _.isNumber ( Any value )
 	
-	// [x.x.x]	Boolean		_.isObject ( Any value )
+	// [x.x.x] Boolean _.isObject ( Any value )
 	
-	// [x.x.x]	Boolean		_.isRegExp ( Any value )
+	// [x.x.x] Boolean _.isRegExp ( Any value )
 	
-	// [x.x.x]	Boolean		_.isString ( Any value )
+	// [x.x.x] Boolean _.isString ( Any value )
 	
-	// [x.x.x]	Boolean		_.isTruthy ( Any value )
+	// [x.x.x] Boolean _.isTruthy ( Any value )
 	
-	// [x.x.x]	Boolean		_.isUndefined ( Any value )
+	// [x.x.x] Boolean _.isUndefined ( Any value )
 	
-	// [x.x.x]	Array		_.keys ( Object object )
+	// [x.x.x] Array _.keys ( Object object )
+	_.keys=O.keys||function(a){
+		
+	};
 	
-	// [0.1.0]	Array		_.map ( Collection collection , Function mapper [ , Any context = root ] )
+	// [0.1.0] Array _.map ( Collection collection , Function mapper [ , Any context = root ] )
 	_.map=function(a,f,c){
 		var r=[];
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=-1,l=a.length;
 			while(++i<l){
 				r.push(f.call(c,a[i],i,a));
@@ -250,11 +270,11 @@
 		return r;
 	};
 	
-	// [0.1.0]	Array		_.map.right ( Collection collection , Function mapper [ , Any context = root ] )
+	// [0.1.0] Array _.map.right ( Collection collection , Function mapper [ , Any context = root ] )
 	_.map.right=function(a,f,c){
 		var r=[];
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=a.length;
 			while(--i>-1){
 				r.push(f.call(c,a[i],i,a));
@@ -263,10 +283,10 @@
 		return r;
 	};
 	
-	// [0.1.0]	Array		_.map.self ( Collection collection , Function mapper [ , Any context = root ] )
+	// [0.1.0] Array _.map.self ( Collection collection , Function mapper [ , Any context = root ] )
 	_.map.self=function(a,f,c){
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=-1,l=a.length;
 			while(++i<l){
 				a[i]=f.call(c,a[i],i,a);
@@ -275,19 +295,16 @@
 		return a;
 	};
 	
-	// [x.x.x]	Array		_.map.right.self ( Collection collection , Function mapper [ , Any context = root ] )
-	// [x.x.x]	Array		_.map.self.right ( Collection collection , Function mapper [ , Any context = root ] )
-	
-	// [0.1.0]	Void		_.noop ( )
+	// [0.1.0] Void _.noop ( )
 	_.noop=function(){};
 	
-	// [x.x.x]	Number		_.now ( )
+	// [x.x.x] Number _.now ( )
 	
-	// [0.1.0]	Variable	_.reduce ( Collection collection , Any memo , Function reducer [ , Any context = root ] )
+	// [0.1.0] Variable _.reduce ( Collection collection , Any memo , Function reducer [ , Any context = root ] )
 	_.reduce=function(a,m,f,c){
 		var r=m;
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=-1,l=a.length;
 			while(++i<l){
 				r=f.call(c,r,a[i],i,a);
@@ -296,11 +313,11 @@
 		return r;
 	};
 	
-	// [0.1.0]	Variable	_.reduce.right ( Collection collection , Any memo , Function reducer [ , Any context = root ] )
+	// [0.1.0] Variable _.reduce.right ( Collection collection , Any memo , Function reducer [ , Any context = root ] )
 	_.reduce.right=function(a,m,f,c){
 		var r=m;
 		if(_.isCollection(a)){
-			c=c||R;
+			c=c||root;
 			var i=a.length;
 			while(--i>-1){
 				r=f.call(c,r,a[i],i,a);
@@ -309,19 +326,19 @@
 		return r;
 	};
 	
-	// [x.x.x]	Object		_.transform ( Object transformed , Object transformer )
+	// [x.x.x] Object _.transform ( Object transformed , Object transformer )
 	
-	// [0.1.0]	Boolean		_.true ( )
+	// [0.1.0] Boolean _.true ( )
 	_["true"]=function(){
 		return true;
 	};
 	
-	// [x.x.x]	String		_.typeOf ( Any value )
+	// [x.x.x] String _.typeOf ( Any value )
 	
-	// [0.1.0]	Number		_.zero ( )
+	// [0.1.0] Number _.zero ( )
 	_.zero=function(){
 		return 0;
 	};
 	
 	R._=_;
-})(this);
+})(this,Object);
