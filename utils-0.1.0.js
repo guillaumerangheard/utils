4c72842866614bad4abce2358b5cc13694ac962b
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2021-03-19 09:20:58 +0100

    Update utils-0.1.0.js

--- utils-0.1.0.js
+++ utils-0.1.0.js
@@ -1,5 +1,8 @@
-(function(root,O){
-	var _tS=function(a){
+(function(root,N,O,U){
+	var _tP=function(a){
+			return a.replace(/.[0-9]+/g,function(){return "["+x+"]";});
+		},
+		_tS=function(a){
 			return O.prototype.toString.call(a);
 		},
 		_={};
@@ -72,6 +75,13 @@
 	};
 	
 	// [x.x.x] Function _.arrow ( String expression )
+	_.arrow=function(a){
+		if(_.isString(a)){
+			var b=a.split("=>");
+			
+		}
+		return _.noop;
+	};
 	
 	// [0.1.0] Number _.avg ( Collection collection [ , Function getter = _.identity ] )
 	// [0.1.0] Number _.average ( Collection collection [ , Function getter = _.identity ] )
@@ -90,6 +100,8 @@
 		return r;
 	}
 	
+	// [x.x.x] Void _.call ( Collection collection , Function callback [ , Array arguments = [] [ , Any context = root ] ] )
+	
 	// [0.1.0] Variable _.const ( Any value )
 	// [0.1.0] Variable _.constant ( Any value )
 	_["const"]=_.constant=function(a){
@@ -97,6 +109,8 @@
 			return a;
 		};
 	};
+	
+	// Boolean _.contains ( Collection collection , Any value [ , Number fromIndex = 0 ] )
 	
 	// [0.1.0] Number _.count ( Collection collection , Function counter [ , Any context = root ] )
 	// -> Boolean counter ( Variable item , Number index , Collection collection )
@@ -148,8 +162,9 @@
 	
 	// [0.1.0] Void _.each.right ( Collection collection , Function iterator [ , Any context = root ] )
 	// -> Boolean iterator ( Variable item , Number index , Collection collection )
+	//// Requires: _.isArrayLike
 	_.each.right=function(a,f,c){
-		if(_.isCollection(a)){
+		if(_.isArrayLike(a)){
 			c=c||root;
 			var i=a.length;
 			while(--i>-1){
@@ -254,7 +269,20 @@
 	
 	// [x.x.x] Function _.getter ( Function getter )
 	// [x.x.x] Function _.getter ( Number index )
+	// [x.x.x] Function _.getter ( String expression )
 	// [x.x.x] Function _.getter ( String path )
+	//// Requires: _.typeOf
+	_.getter=function(a){
+		switch(_.typeOf(a)){
+			case"function":return a;
+			case"number":return new Function("a","return a["+a+"];");
+			case"String":return -1<a.indexOf("=>")?_.arrow(a):new Function("a","return a."+_tP(a)+";");
+			default:return _.identity
+		}
+	};
+	
+	// [x.x.x] Object _.group ( Collection collection , Function getter [ , Any context = root ] )
+	// [x.x.x] Object _.group ( Collection collection , String path [ , Any context = root ] )
 	
 	// [0.1.0] Variable _.identity ( Any value )
 	_.identity=function(a){
@@ -313,9 +341,11 @@
 	};
 	
 	// [0.1.0] Boolean _.isNaN ( Any value )
-	_.isNaN=function(a){
+	_.isNaN=N.isNaN|function(a){
 		return a!==a;
 	};
+	
+	// [x.x.x] Boolean _.isNil ( Any value )
 	
 	// [0.1.0] Boolean _.isNull ( Any value )
 	_.isNull=function(a){
@@ -368,6 +398,8 @@
 	};
 	
 	// [0.1.0] Array _.map ( Collection collection , Function mapper [ , Any context = root ] )
+	// [0.1.0] Array _.map ( Collection collection , Number mapper [ , Any context = root ] )
+	// [0.1.0] Array _.map ( Collection collection , String mapper [ , Any context = root ] )
 	//// Requires: _.isArrayLike
 	_.map=function(a,f,c){
 		var r=[];
@@ -410,10 +442,17 @@
 		return a;
 	};
 	
+	// [x.x.x] Number _.max ( Collection collection [ , Function getter = _.identity ] )
+	
+	// [x.x.x] Number _.min ( Collection collection [ , Function getter = _.identity ] )
+	
 	// [0.1.0] Void _.noop ( )
 	_.noop=function(){};
 	
 	// [x.x.x] Number _.now ( )
+	
+	// [x.x.x] Array _.pick ( Collection collection , String path )
+	//// Cf. underscore's _.pluck.
 	
 	// [0.1.0] Variable _.reduce ( Collection collection , Any memo , Function reducer [ , Any context = root ] )
 	//// Requires: _.isArrayLike
@@ -444,25 +483,9 @@
 		return r;
 	};
 	
-	// [0.1.0] Object _.transform ( Object transformed , Object transformer [ , onlyExplicit = false ] )
-	_.transform=function(a,b,e){
-		var r={};
-		if(e){
-			_.each.
-		}
-		else{
-			var K=_.keys(b);
-			_.each.key(a,function(v,k){
-				if(-1<K.indexOf(k)){
-					
-				}
-				else{
-					r[k]=v;
-				}
-			});
-		}
-		return r;
-	};
+	// [x.x.x] Array _.sort ( Collection collection , Function sorter [ , Any context = root ] )
+	
+	// [x.x.x] Object _.transform ( Object transformed , Object transformer [ , onlyExplicit = false ] )
 	
 	// [0.1.0] Boolean _.true ( )
 	_["true"]=function(){
@@ -471,10 +494,11 @@
 	
 	// [x.x.x] String _.typeOf ( Any value )
 	
+	
 	// [0.1.0] Number _.zero ( )
 	_.zero=function(){
 		return 0;
 	};
 	
-	R._=_;
-})(this,Object);
+	root._=_;
+})(this,Number,Object);
